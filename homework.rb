78d301a280639fe473873088e93f0a45babd34d7
Kevin Cruz <kcruz538@gmail.com>
2014-09-29 14:33:20 -0500

    process_records.rb

---
+++ homework.rb
@@ -0,0 +1,18 @@
+# class Shipment
+#   attr_accessor :destination, :what_got_shipped, :how_many, :money
+#
+#   attr_accessor :destination
+#   attr_accessor :what_got_shipped
+#   attr_accessor :how_many
+#   attr_accessor :money
+
+end
+
+class Shipment
+  attr_accessor :destination, :what_got_shipped, :how_many, :money
+
+  attr_accessor :destination
+  attr_accessor :what_got_shipped
+  attr_accessor :how_many
+  attr_accessor :money
+end 
--- process_records.rb
+++ process_records.rb
@@ -1,6 +1,7 @@
 # Record format
 # Destination, What got shipped, Number of crates, Money we made
 
+
 # Good news Rubyists!
 # We have a week of records tracking what we shipped at Planet Express.
 # I need you to answer a few questions for Hermes.
@@ -21,7 +22,103 @@
 # How much of a bonus did each employee get?
 
 # BONUS - uses classes for each shipment; but not necessary
+# class Shipment
+#   attr_accessor :destination, :what_got_shipped, :how_many, :money
+#
+#   attr_accessor :desination
+#   attr_accessor :what_got_shipped
+#   attr_accessor :how_many
+#   attr_accessor :money
+# end
+
+class Shipment
+  attr_accessor :destination, :what_got_shipped, :how_many, :money
+
+  attr_accessor :destination
+  attr_accessor :what_got_shipped
+  attr_accessor :how_many
+  attr_accessor :money
+end
+
+puts "Hello World"
+
+
+# puts "hai"
+
+shipments = []
+
+money_bonus = 0
+fry_bonus = 0
+amy_bonus = 0
+bender_bonus = 0
+leela_bonus = 0
+
+
+
+# open("planet_express_logs").each do |line|
+#   p line.chomp
 
 open("planet_express_logs").each do |line|
   p line.chomp
+
+  values_array = line.chomp.split(",")
+
+  shipment = Shipment.new
+  shipment.destination = values_array[0]
+  shipment.what_got_shipped = values_array[1]
+  shipment.how_many = values_array[2].to_i
+  shipment.money = values_array[3].to_i
+
+  # puts "the values array looks like #{values_array}"
+  money_bonus += shipment.money
+
+  if shipment.destination == "Earth"
+    fry_bonus += shipment.money * 0.10
+  elsif shipment.destination == "Mars"
+    amy_bonus += shipment.money * 0.10
+  elsif shipment.destination == "Uranus"
+    bender_bonus += shipment.money * 0.10
+  else
+    leela_bonus += shipment.money * 0.10
+  end
+  shipments << shipment
+
 end
+
+
+puts "How much money did we make this week?"
+puts "Money #{money_bonus}"
+
+puts "fry_bonus: #{fry_bonus}"
+puts "amy_bonus: #{amy_bonus}"
+puts "bender_bonus: #{bender_bonus}"
+puts "leela_bonus: #{leela_bonus}"
+
+puts "shipment : #{shipments}"
+
+
+
+
+
+# records = []
+#
+# open("planet_express_logs").each do |line|
+#   words = []
+#   word = ""
+#   records = line.chomp.chars
+#
+#   records.each do |x|
+#     if x == ","
+#       words << word
+#       word = ""
+#     else
+#       word = word + x
+#     end
+#   end
+# words << word
+# p word
+# p words
+#   records << words
+# end
+#
+# p records
